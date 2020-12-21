776ebdab8d4aa6af0497b3262e73669e012e5d42
Vikalp Patil <46565785+vikalp174@users.noreply.github.com>
2020-12-22 00:34:22 +0530

    Update recomentdations.py

--- recomentdations.py
+++ recomentdations.py
@@ -2,6 +2,6 @@ def recomend(mat_vector, num):
     main_list = mat_vector[1]
     main_list.sort(key=lambda x: x[1], reverse=True)
     snd = []
-    for i in range(0, num+1):
+    for i in range(1, num+1):
         snd.append(main_list[i][0])
     return snd
